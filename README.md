acefb67fb81978edd0d2297f11ae7658b7484181
Chris Seymour <chris.j.seymour@hotmail.com>
2013-10-10 18:50:23 +0100

    added twitter support

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ### tweetdns
 
 A python service that tweets when your dynamic IP changes.
+
+### dependencies
+
+Python twitter API https://github.com/sixohsix/twitter
+
+    $ easy_install twitter
+
+### Steps
+
+ - Create new twitter account for tweeting your IP
+ - Make account private [optional]
+ - Register account with https://dev.twitter.com/
+ - Create new application to get a Consumer Key and Secret
+ - Add Key and Secret to Tweeter class in tweetdns.py
+ - Start daemon `./tweetdns start`
+ - Follow your new twitter account to get updates when your IP changes
--- tweetdns.py
+++ tweetdns.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import time
 import urllib2
 from daemon import Daemon
+from twitter import oauth_dance, read_token_file, OAuth, Twitter
 
 
 class ExternalIP(object):
@@ -30,20 +32,54 @@ class ExternalIP(object):
         return False
 
 
+class Tweeter(object):
+
+    def __init__(self):
+
+        CONSUMER_KEY = None
+        CONSUMER_SECRET = None
+
+        if not (CONSUMER_KEY and CONSUMER_SECRET):
+            raise Exception("Whoa! Fill in your keys first!")
+
+        TWITTER_CREDS = os.path.expanduser('~/.tweetdns')
+
+        if not os.path.exists(TWITTER_CREDS):
+            oauth_dance("tweetdns", CONSUMER_KEY, CONSUMER_SECRET, TWITTER_CREDS)
+
+        oauth_token, oauth_secret = read_token_file(TWITTER_CREDS)
+        tauth = OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET)
+        self.twitter = Twitter(auth=tauth)
+
+    def tweet(self, ip):
+        '''
+        Tweet new IP address
+        '''
+        self.twitter.statuses.update(status=ip)
+
+
 class TweetDNSDaemon(Daemon):
+
+    def nest(self, tweeter):
+        '''
+        Initialise a tweeter before daemonizing
+        '''
+        self.tweet = tweeter()
 
     def run(self):
         ip = ExternalIP()
         while True:
-            time.sleep(5)
             if ip.newAddress():
-                print "New IP %s" % ip.address
+                self.tweet.tweet("New IP %s" % ip.address)
+            time.sleep(3600)
 
 
 def main():
+
     daemon = TweetDNSDaemon('/tmp/tweetdns.pid')
     if len(sys.argv) > 1:
         if 'start' == sys.argv[1]:
+            daemon.nest(Tweeter)
             daemon.start()
         elif 'stop' == sys.argv[1]:
             daemon.stop()
